5a37c4bdef0b9dc1e7a6d3e8413d968913324282
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-01-02 18:46:38 -0800

    feat(about-us): add about us component
    created about us components with styles and media queries

--- src/components/marketingPage/Navbar.tsx
+++ src/components/marketingPage/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { MdLocationOn } from "react-icons/md";
+// import { MdLocationOn } from "react-icons/md";
 import { ImUserPlus } from "react-icons/im";
 import { FaUsers } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+// import { MdEmail } from "react-icons/md";
 import { BsFillLockFill } from "react-icons/bs";
 import logo from '../../imgs/logo.jpg';
 
@@ -13,18 +13,21 @@ const Navbar = () => {
          <div className="inner-wrapper">
             <nav className="first-nav">
                <div className="t">
-                  <NavLink to="/locations" activeClassName="active">
+                  {/* <NavLink to="/locations" activeClassName="active">
                      <span><MdLocationOn /></span>
-                     Find ATMs & Branches</NavLink>
+                     Find ATMs & Branches
+                  </NavLink> */}
                   <NavLink to="/register" activeClassName="active">
                      <span><ImUserPlus /></span>
                      Open a New Account</NavLink>
-                  <NavLink to="/about" activeClassName="active">
+                  <NavLink to="/about-us" activeClassName="active">
                      <span><FaUsers /></span>
-                     About us</NavLink>
-                  <NavLink to="/about" activeClassName="active">
+                     About us
+                  </NavLink>
+                  {/* <NavLink to="/about" activeClassName="active">
                      <span><MdEmail /></span>
-                     Contact Us</NavLink>
+                     Contact Us
+                  </NavLink> */}
                </div>
             </nav>
             <nav className="second-nav">
@@ -32,11 +35,11 @@ const Navbar = () => {
                   <img src={logo} alt="logo"/>
                </Link>
                <div className="second-nav-wrapper">
-                  <NavLink to="/bankin" activeClassName="active">Banking</NavLink>
+                  {/* <NavLink to="/bankin" activeClassName="active">Banking</NavLink> */}
                   <NavLink to="/loans" activeClassName="active">Loans</NavLink>
-                  <NavLink to="/mortage" activeClassName="active">Mortage</NavLink>
+                  {/* <NavLink to="/mortage" activeClassName="active">Mortage</NavLink>
                   <NavLink to="/servicer" activeClassName="active">Services</NavLink>
-                  <NavLink to="/support" activeClassName="active">Support</NavLink>
+                  <NavLink to="/support" activeClassName="active">Support</NavLink> */}
                   <NavLink to="/login" activeClassName="active"><span><BsFillLockFill /></span> Log in</NavLink>
                </div>
             </nav>
