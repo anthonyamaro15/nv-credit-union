0d3ff833cd88919f7ecbb9702d694fc875fb8bb1
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-01-03 13:50:48 -0800

    feat(create-account): add validation to create acc form
    added validation to create account forms

--- src/components/account/createAccountForms/ContactInformation.tsx
+++ src/components/account/createAccountForms/ContactInformation.tsx
@@ -22,7 +22,9 @@ interface FormProps {
 }
 
 const ContactInformation = () => {
-   const { register, handleSubmit, errors } = useForm<FormProps>();
+   const { register, handleSubmit, errors } = useForm<FormProps>({
+      mode: "onBlur"
+   });
    const history = useHistory();
 
    const onSubmit = (values: any) => {
@@ -33,28 +35,49 @@ const ContactInformation = () => {
    return (
       <section className="PersonalInformation">
          <h6>Please enter your contact information in the fields below.</h6>
-         {/* <h1>mailing address</h1> */}
          <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="addressLineOne">Address Line 1
-               <input type="text" name="addressLineOne" id="addressLineOne" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="addressLineOne" 
+                  id="addressLineOne" 
+                  ref={register({ required: true })} 
+               />
             </label>
             {errors.addressLineOne && <p className="errors">Field require</p>}
             <label htmlFor="addressLineTwo">Address Line 2
-               <input type="text" name="addressLineTwo" id="addressLineTwo" ref={register} />
+               <input 
+                  type="text" 
+                  name="addressLineTwo" 
+                  id="addressLineTwo" 
+                  ref={register} 
+               />
             </label>
             <label htmlFor="apartmentNumber">Apartment Number
-               <input type="text" name="apartmentNumber" id="apartmentNumber" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="apartmentNumber" 
+                  id="apartmentNumber" 
+               />
             </label>
-            {errors.apartmentNumber && <p className="errors">Field require</p>}
-
             <div className="city-state-zip-wrapper">
                <span>City, State, ZIP</span>
                <div className="input-wrappers">
                   <label htmlFor="city">
-                     <input type="text" name="city" id="city" placeholder="City" ref={register({ required: true })} />
+                     <input 
+                        type="text" 
+                        name="city" 
+                        id="city" 
+                        placeholder="City" 
+                        ref={register({ required: true })} 
+                     />
                   </label>
                   <label htmlFor="state">
-                     <select name="state" id="state" ref={register({ required: true })}>
+                     <select 
+                        name="state" 
+                        id="state" 
+                        ref={register({ required: true })}
+                     >
                         <option value="">State</option>
                         {states.map((single: string, i) => (
                            <option value={single} key={i}>{single}</option>
@@ -62,14 +85,29 @@ const ContactInformation = () => {
                      </select>
                   </label>
                   <label htmlFor="zip">
-                     <input type="text" name="zip" id="zip" placeholder="ZIP code" ref={register({ required: true })} />
+                     <input 
+                        type="text" 
+                        name="zip" 
+                        id="zip" 
+                        placeholder="ZIP code" 
+                        maxLength={5} 
+                        ref={register({ 
+                           required: true,
+                           pattern: /^\d+$/
+                        })} 
+                     />
                   </label>
                </div>
             </div>
-            {errors.city && <p className="errors">Fields require</p>}
+            <p className="errors">{errors.zip && errors.zip.type === "required" && "Required field"}</p>
+            <p className="errors">{errors.zip && errors.zip.type === "pattern" && "Enter valid Zipcode"}</p>
 
             <label htmlFor="country">Country
-               <select name="country" id="country" ref={register({ required: true })}>
+               <select 
+                  name="country" 
+                  id="country" 
+                  ref={register({ required: true })}
+               >
                   <option value="">- Select One -</option>
                   <option value="unitedStates">United States</option>
                </select>
@@ -77,7 +115,11 @@ const ContactInformation = () => {
             {errors.country && <p className="errors">Field require</p>}
 
             <label htmlFor="citizenshipType">Citizenship Type
-               <select name="citizenshipType" id="citizenshipType" ref={register({ required: true })}>
+               <select 
+                  name="citizenshipType" 
+                  id="citizenshipType" 
+                  ref={register({ required: true })}
+               >
                   <option value=""></option>
                   <option value="usCitizen">US Citizen</option>
                   <option value="residentAlien">Resident Alien</option>
@@ -85,28 +127,70 @@ const ContactInformation = () => {
             </label>
             {errors.citizenshipType && <p className="errors">Field require</p>}
             <label htmlFor="primaryPhone">Primary Phone number
-               <input type="text" name="primaryPhone" id="primaryPhone" placeholder="xxx-xxx-xxxx" ref={register({ required: true })} />
+               <input 
+                  type="tel" 
+                  name="primaryPhone" 
+                  id="primaryPhone" 
+                  maxLength={10}
+                  ref={register( { required: true, pattern: /^\d+$/ })} 
+               />
             </label>
-            {errors.primaryPhone && <p className="errors">Field require</p>}
+            <p className="errors">
+               {errors.primaryPhone && errors.primaryPhone.type === "required" && "Required field"}
+            </p>
+            <p className="errors">
+               {errors.primaryPhone && errors.primaryPhone.type === "pattern" && "Enter valid phone number"}
+            </p>
 
             <label htmlFor="cellPhone">Cell Phone
-               <input type="text" name="cellPhone" id="cellPhone" placeholder="xxx-xxx-xxxx" ref={register({ required: true })} />
+               <input 
+                  type="tel" 
+                  name="cellPhone" 
+                  id="cellPhone" 
+                  maxLength={10}
+                  ref={register( { pattern: /^\d+$/ })} 
+               />
             </label>
-            {errors.cellPhone && <p className="errors">Field require</p>}
-
+            <p className="errors">
+               {errors.cellPhone && errors.cellPhone.type === "required" && "Required field"}
+            </p>
+            <p className="errors">
+               {errors.cellPhone && errors.cellPhone.type === "pattern" && "Enter valid phone number"}
+            </p>
             <label htmlFor="workPhone">Work Phone
-               <input type="text" name="workPhone" id="workPhone" placeholder="xxx-xxx-xxxx" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="workPhone" 
+                  id="workPhone" 
+                  maxLength={10}
+                  ref={register( { pattern: /^\d+$/ })} 
+               />
             </label>
-            {errors.workPhone && <p className="errors">Field require</p>}
-
-
+            <p className="errors">
+               {errors.workPhone && errors.workPhone.type === "pattern" && "Enter valid phone number"}
+            </p>
             <label htmlFor="driverLincenseNumber">Driver's License Number
-               <input type="text" name="driverLincenseNumber" id="driverLincenseNumber" placeholder="xxx-xxx-xxxx" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="driverLincenseNumber" 
+                  id="driverLincenseNumber" 
+                  maxLength={10}
+                  ref={register( { required: true, pattern: /^\d+$/ })} 
+               />
             </label>
-            {errors.driverLincenseNumber && <p className="errors">Field require</p>}
+            <p className="errors">
+               {errors.driverLincenseNumber && errors.driverLincenseNumber.type === "required" && "Field require"}
+            </p>
+            <p className="errors">
+               {errors.driverLincenseNumber && errors.driverLincenseNumber.type === "pattern" && "Enter valid ID number"}
+            </p>
 
             <label htmlFor="licenseState">state
-               <select name="licenseState" id="licenseState" ref={register({ required: true })}>
+               <select 
+                  name="licenseState" 
+                  id="licenseState" 
+                  ref={register({ required: true })}
+               >
                   <option value="">-Select One-</option>
                   {states.map((single: string, i) => (
                      <option value={single} key={i}>{single}</option>
@@ -116,14 +200,27 @@ const ContactInformation = () => {
             {errors.licenseState && <p className="errors">Field require</p>}
 
             <label htmlFor="dateIssued">Date Issued 
-               <input type="text" name="dateIssued" id="dateIssued" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="dateIssued" 
+                  id="dateIssued" 
+                  placeholder="mm/dd/yyyy"
+                  maxLength={10}
+                  ref={register({ required: true })} 
+               />
             </label>
-            {errors.dateIssued && <p className="errors">Field require</p>}
+            <p className="errors">{errors.dateIssued && "Field require"}</p>
             <label htmlFor="expirationDate">Expiration Date
-               <input type="text" name="expirationDate" id="expirationDate" ref={register({ required: true })} />
+               <input 
+                  type="text" 
+                  name="expirationDate" 
+                  id="expirationDate" 
+                  placeholder="mm/dd/yyyy"
+                  maxLength={10}
+                  ref={register({ required: true })} 
+               />
             </label>
-            {errors.expirationDate && <p className="errors">Field require</p>}
-
+            <p className="errors">{errors.expirationDate && "Field require"}</p>
             <div className="btn-wrapper">
                <button type="submit">next</button>
             </div>
